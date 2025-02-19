42dcf837f0791019583f7f48b1e4f13e7bde0e2f
Justin Crittenden <124092904+Jcritt1005@users.noreply.github.com>
2025-02-19 15:15:14 -0800

    I added the files using command line git lets git it

---
+++ Code/script.js
@@ -0,0 +1,14 @@
+// Justin Crittenden - February 18, 2025
+const submitButton = document.getElementById("submit");
+const username = document.getElementById("user");
+const password = document.getElementById("pass");
+
+const isValid = () => {
+    let user = username.textContent;
+    let pass = password.textContent;
+
+    // Match login info with accounts
+    
+}
+
+submitButton.addEventListener("click", isValid);
