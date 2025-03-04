870f411ab57e5f7ec09d90f7d924071b1342d8c9
Orlando Trujillo-Ortiz <orlandotrujillo573@gmail.com>
2025-03-04 01:30:29 -0800

    reports.ejs set up with basic dynamic generation and some basic css

--- Code/index.js
+++ Code/index.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const app = express()
 const path = require('path');
+const {reportTypes} = require("./reportsLogic");
 const port = 3000
 
 app.use(express.static(__dirname + '/public'));
@@ -13,13 +14,18 @@ app.get('/', (req, res) => {
 });
 
 app.post('/', (req, res) =>{
-    res.render("test",{})
+
 });
 
 // THIS SHOULD NOT BE A GET PROBABLY MAYBE UNLESS???
 app.get('/result', (req, res) => {
+    // TODO: put logic here obviously
     res.render("results");
 });
+
+app.get("/reports", (req, res) =>{
+    res.render("reports", {availableReportsMap: reportTypes});
+})
 
 /* Port Number should be an environment variable fyi */
 app.listen(port, () => {
---
+++ Code/reportsLogic.js
@@ -0,0 +1,5 @@
+export const reportTypes = new Map([
+    ["mailList", "Mailing List"],
+    ["expPurchaseInMonth", "Purchase of Services Expiring Soon"],
+    ["listAllClients", "List of All Clients"]
+]);
