10b03a025db5767dc70ab3fe80f7aa463ef96ac2
Orlando Trujillo-Ortiz <orlandotrujillo573@gmail.com>
2025-03-28 18:40:51 -0700

    Fix type annotations for programs and refactor ClientBuilder tests for clarity

--- Code/tests/ClientBuilder.test.js
+++ Code/tests/ClientBuilder.test.js
@@ -19,115 +19,93 @@ describe("ClientBuilder Object Creation", () => {
 })
 
 describe("ClientBuilder Setters", () => {
-    let theClient;
+    let theBuilder;
     beforeAll(() => {
-        theClient = new ClientBuilder()
-            .setMiddleName("Abigail")
-            .setLastName("Auger")
-            .setPronouns("She/Her")
-            .setPhoneNumber("707-777-7777")
-            .setAddress(new Address("123 A St.", "Arcata", "California", "95521"))
-            .setEmail("alice@foobar.com")
-            .setDOB(new Date("2000-06-06"))
-            .setSex("Female")
-            .setMaritalStatus("Single")
-            .setPreferredHospital("St. Joseph")
-            .setPrimaryInsurance(new Insurance("Medical", "12345"))
-            .setSecondaryInsurance(new Insurance("Blue Cross", "67890"))
-            .setPcp(new ContactInfo("Open Door", "777-777-7777", "456 B St Arcata, CA"))
-            .setPrimaryPhysician(new ContactInfo("Dr. Ron Bon", "777-777-8888", "456 B St Arcata, CA"))
-            .setMedicationList([
-                new Medication("Codine", false, "10mL", "Twice a day", "Cough",
-                    "None", "Dr. Phil"),
-                new Medication("Vitamin D", true, "5mg", "Once a day",
-                    "Deficiency", "Irritability", "Dr. Foobar")
-            ])
-            .setLikes("Likes to go on walks. Likes cats. Likes sand")
-            .setDislikes("Hates cows. Hates cereal. Dislikes milk")
-            .setGoals("Go to the moon. Go to Ohio")
-            .setHobbies("Calculus. Knitting. Video Games")
-            .setAchievements("Won the 2012 Olympic Games in Boxing")
-            .setMiscNotes("Known to steal cheese")
-            .setSupportTeam([
-                new SupportStaff("Howard Hill", "Worker 2", 123, new Date("2005-05-06"),
-                    new Date("2012-03-04")),
-                new SupportStaff("Nigel Noodle", "Worker 1", 456, new Date("2012-03-04"))
-            ])
-            .setCaseNoteList([
-                new CaseNote("Spent all their weekly budget", new Date("2025-01-25"), "Bob Saget",
-                    false, true),
-                new CaseNote("Got into verbal dispute at home", new Date("2023-06-14"),
-                    "Mary Hawthorn", true, false)
-            ])
-            .setPictureURL("https://upload.wikimedia.org/wikipedia/commons/0/05/Bob_Saget_1987.jpg")
-            .setPrograms([
-                new Programs("Bay Center"),
-                new Programs("Clinical Services")
-            ])
-            .build();
+        theBuilder = new ClientBuilder();
     });
 
     test("firstName", () => {
-        expect(theClient.getFirstName()).toBe("Alice")
+        let theClient = theBuilder.setFirstName("Alice").build();
+        expect(theClient.getFirstName()).toBe("Alice");
     });
 
     test("middleName", () => {
-        expect(theClient.getMiddleName()).toBe("Abigail")
+        let theClient = theBuilder.setMiddleName("Abigail").build();
+        expect(theClient.getMiddleName()).toBe("Abigail");
     });
 
     test("lastName", () => {
-        expect(theClient.getLastName()).toBe("Auger")
+        let theClient = theBuilder.setLastName("Auger").build();
+        expect(theClient.getLastName()).toBe("Auger");
     });
 
     test("pronouns", () => {
-        expect(theClient.getPronouns()).toBe("She/Her")
+        let theClient = theBuilder.setPronouns("She/Her").build();
+        expect(theClient.getPronouns()).toBe("She/Her");
     });
 
     test("phoneNumber", () => {
-        expect(theClient.getPhoneNumber()).toBe("707-777-7777")
+        let theClient = theBuilder.setPhoneNumber("707-777-7777").build();
+        expect(theClient.getPhoneNumber()).toBe("707-777-7777");
     });
 
     test("Address", () => {
-        expect(theClient.getAddress()).toStrictEqual(new Address("123 A St.", "Arcata", "California", "95521"))
+        let theClient = theBuilder.setAddress(new Address("123 A St.", "Arcata", "California", "95521")).build();
+        expect(theClient.getAddress()).toStrictEqual(new Address("123 A St.", "Arcata", "California", "95521"));
     });
 
     test("email", () => {
-        expect(theClient.getEmail()).toBe("alice@foobar.com")
+        let theClient = theBuilder.setEmail("alice@foobar.com").build();
+        expect(theClient.getEmail()).toBe("alice@foobar.com");
     });
 
     test("dob", () => {
-        expect(theClient.getDOB()).toStrictEqual(new Date("2000-06-06"))
+        let theClient = theBuilder.setDOB(new Date("2000-06-06")).build();
+        expect(theClient.getDOB()).toStrictEqual(new Date("2000-06-06"));
     });
 
     test("sex", () => {
-        expect(theClient.getSex()).toBe("Female")
+        let theClient = theBuilder.setSex("Female").build();
+        expect(theClient.getSex()).toBe("Female");
     });
 
     test("maritalStatus", () => {
-        expect(theClient.getMaritalStatus()).toBe("Single")
+        let theClient = theBuilder.setMaritalStatus("Single").build();
+        expect(theClient.getMaritalStatus()).toBe("Single");
     });
 
     test("preferredHospital", () => {
-        expect(theClient.getPreferredHospital()).toBe("St. Joseph")
+        let theClient = theBuilder.setPreferredHospital("St. Joseph").build();
+        expect(theClient.getPreferredHospital()).toBe("St. Joseph");
     });
 
     test("primaryInsurance", () => {
-        expect(theClient.getPrimaryInsurance()).toStrictEqual(new Insurance("Medical", "12345"))
+        let theClient = theBuilder.setPrimaryInsurance(new Insurance("Medical", "12345")).build();
+        expect(theClient.getPrimaryInsurance()).toStrictEqual(new Insurance("Medical", "12345"));
     });
 
     test("secondaryInsurance", () => {
-        expect(theClient.getSecondaryInsurance()).toStrictEqual(new Insurance("Blue Cross", "67890"))
+        let theClient = theBuilder.setSecondaryInsurance(new Insurance("Blue Cross", "67890")).build();
+        expect(theClient.getSecondaryInsurance()).toStrictEqual(new Insurance("Blue Cross", "67890"));
     });
 
     test("pcp", () => {
-        expect(theClient.getPcp()).toStrictEqual(new ContactInfo("Open Door", "777-777-7777", "456 B St Arcata, CA"))
+        let theClient = theBuilder.setPcp(new ContactInfo("Open Door", "777-777-7777", "456 B St Arcata, CA")).build();
+        expect(theClient.getPcp()).toStrictEqual(new ContactInfo("Open Door", "777-777-7777", "456 B St Arcata, CA"));
     });
 
     test("primaryPhysician", () => {
+        let theClient = theBuilder.setPrimaryPhysician(new ContactInfo("Dr. Ron Bon", "777-777-8888", "456 B St Arcata, CA")).build();
         expect(theClient.getPrimaryPhysician()).toStrictEqual(new ContactInfo("Dr. Ron Bon", "777-777-8888", "456 B St Arcata, CA"))
     });
 
     test("medicationList", () => {
+        let theClient = theBuilder.setMedicationList([
+            new Medication("Codine", false, "10mL", "Twice a day", "Cough",
+                "None", "Dr. Phil"),
+            new Medication("Vitamin D", true, "5mg", "Once a day",
+                "Deficiency", "Irritability", "Dr. Foobar")
+        ]).build();
         expect(theClient.getMedicationList()).toStrictEqual([
             new Medication("Codine", false, "10mL", "Twice a day", "Cough",
                 "None", "Dr. Phil"),
@@ -137,30 +115,41 @@ describe("ClientBuilder Setters", () => {
     });
 
     test("likes", () => {
-        expect(theClient.getLikes()).toBe("Likes to go on walks. Likes cats. Likes sand")
+        let theClient = theBuilder.setLikes("Likes to go on walks. Likes cats. Likes sand").build();
+        expect(theClient.getLikes()).toBe("Likes to go on walks. Likes cats. Likes sand");
     });
 
     test("dislikes", () => {
-        expect(theClient.getDislikes()).toBe("Hates cows. Hates cereal. Dislikes milk")
+        let theClient = theBuilder.setDislikes("Hates cows. Hates cereal. Dislikes milk").build();
+        expect(theClient.getDislikes()).toBe("Hates cows. Hates cereal. Dislikes milk");
     });
 
     test("goals", () => {
-        expect(theClient.getGoals()).toBe("Go to the moon. Go to Ohio")
+        let theClient = theBuilder.setGoals("Go to the moon. Go to Ohio").build();
+        expect(theClient.getGoals()).toBe("Go to the moon. Go to Ohio");
     });
 
     test("hobbies", () => {
+        let theClient = theBuilder.setHobbies("Calculus. Knitting. Video Games").build();
         expect(theClient.getHobbies()).toBe("Calculus. Knitting. Video Games")
     });
 
     test("achievements", () => {
+        let theClient = theBuilder.setAchievements("Won the 2012 Olympic Games in Boxing").build();
         expect(theClient.getAchievements()).toBe("Won the 2012 Olympic Games in Boxing")
     });
 
     test("miscNotes", () => {
+        let theClient = theBuilder.setMiscNotes("Known to steal cheese").build();
         expect(theClient.getMiscNotes()).toBe("Known to steal cheese")
     });
 
     test("supportTeam", () => {
+        let theClient = theBuilder .setSupportTeam([
+            new SupportStaff("Howard Hill", "Worker 2", 123, new Date("2005-05-06"),
+                new Date("2012-03-04")),
+            new SupportStaff("Nigel Noodle", "Worker 1", 456, new Date("2012-03-04"))
+        ]).build();
         expect(theClient.getSupportTeam()).toStrictEqual([
             new SupportStaff("Howard Hill", "Worker 2", 123, new Date("2005-05-06"),
                 new Date("2012-03-04")),
@@ -169,6 +158,12 @@ describe("ClientBuilder Setters", () => {
     });
 
     test("caseNoteList", () => {
+        let theClient = theBuilder.setCaseNoteList([
+            new CaseNote("Spent all their weekly budget", new Date("2025-01-25"), "Bob Saget",
+                false, true),
+            new CaseNote("Got into verbal dispute at home", new Date("2023-06-14"),
+                "Mary Hawthorn", true, false)
+        ]).build();
         expect(theClient.getCaseNoteList()).toStrictEqual([
             new CaseNote("Spent all their weekly budget", new Date("2025-01-25"), "Bob Saget",
                 false, true),
@@ -178,10 +173,15 @@ describe("ClientBuilder Setters", () => {
     });
 
     test("pictureURL", () => {
+        let theClient = theBuilder.setPictureURL("https://upload.wikimedia.org/wikipedia/commons/0/05/Bob_Saget_1987.jpg").build();
         expect(theClient.getPictureURL()).toBe("https://upload.wikimedia.org/wikipedia/commons/0/05/Bob_Saget_1987.jpg")
     });
 
     test("programs", () => {
+        let theClient = theBuilder.setPrograms([
+            new Programs("Bay Center"),
+            new Programs("Clinical Services")
+        ]).build();
         expect(theClient.getPrograms()).toStrictEqual([
             new Programs("Bay Center"),
             new Programs("Clinical Services")
