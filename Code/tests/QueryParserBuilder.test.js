5d0de0a7bf80299f36486a68ce6e87f7dceb1712
Orlando Trujillo-Ortiz <orlandotrujillo573@gmail.com>
2025-03-29 19:04:46 -0700

    Fix type annotation for programs in ClientBuilder; update package.json to include Jest globals and enhance QueryParserBuilder tests

--- Code/tests/QueryParserBuilder.test.js
+++ Code/tests/QueryParserBuilder.test.js
@@ -1,12 +1,35 @@
-import QueryParserBuilder from "../objects/QueryParserBuilder.js";
-import QueryParser from "../objects/QueryParser.js";
+import {jest} from '@jest/globals';
+
+jest.unstable_mockModule('@google-cloud/cloud-sql-connector', ()=>({
+    Connector: jest.fn().mockImplementation(()=>({
+        getOptions: jest.fn(),
+        close: jest.fn()
+    }))
+}));
+
+jest.unstable_mockModule('mysql2/promise', ()=>({
+    default: {
+        createPool: jest.fn()
+    }
+}));
+
+const {connectorMock}= await import('@google-cloud/cloud-sql-connector');
+const mysql = (await import('mysql2/promise')).default;
+const QueryParserBuilder = (await import('../objects/QueryParserBuilder.js')).default;
+const QueryParser = (await import('../objects/QueryParser.js')).default;
+
+const oldDBInstance = process.env.DB_INSTANCE;
 
 describe('QueryParserBuilder Happy Path Tests', () =>{
+
     test("Creation of a QueryParserBuilder object", ()=>{
         expect(new QueryParserBuilder()).toBeInstanceOf(QueryParserBuilder);
     });
 
     test("build() creates a QueryParser object Assuming All Env Vars are set", async ()=>{
+        if (!process.env.DB_INSTANCE){
+            process.env.DB_INSTANCE = "foo:bar:baz";
+        }
 
         try {
             await expect(new QueryParserBuilder().build()).resolves.toBeInstanceOf(QueryParser);
@@ -14,13 +37,22 @@ describe('QueryParserBuilder Happy Path Tests', () =>{
             throw e;
         }
     });
+
+
 });
 
 describe("QueryParserBuilder Error Handling Tests", ()=>{
+    let qpb;
     beforeEach(()=>{
-
+        qpb = new QueryParserBuilder();
     })
-    test("Error is thrown when no process.env.DB_INSTANCE is set", async ()=>{
 
+    afterAll(()=>{
+        process.env.DB_INSTANCE = oldDBInstance;
+    })
+
+    test.skip("Error is thrown when no process.env.DB_INSTANCE is set", async ()=>{
+        process.env.DB_INSTANCE = undefined;
+        await expect(qpb.build()).resolves.toThrowError("DB_INSTANCE env variable is undefined despite being required!")
     })
 });
