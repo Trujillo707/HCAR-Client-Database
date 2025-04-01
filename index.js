f57db5732090cc2f4237255c57b4610b61ffcb3f
Carson Gustafson <carsongustafson@hotmail.com>
2025-04-01 15:31:33 -0700

    Implemented an isAdmin function to check if an accountID is admin.
    Implemented the /api/getAllClients endpoint to retrieve limited client info on all users associated with the currently logged in user. Limited client info from all users is retrieved if the current user is admin.
    
    Added comments and slightly refactored the /api/auth endpoint.

--- index.js
+++ index.js
@@ -34,6 +34,97 @@ app.listen(port, () => {
   console.log(`helloworld: listening on port ${port}`);
 });
 
+/*
+  /api/getAllClients
+
+  Purpose:
+    Returns limited info for each client associated with currently logged in user.
+    Returns limited info for all clients if currently logged in user is admin
+
+  TODO: Change SQL statements to select fewer columns instead of client.*
+*/
+app.get('/api/getAllClients', async (req, res) => {
+  const pool = await createTcpPool({ connectionLimit: 5 }); //Initializes connection to DB
+  try{
+    const accountID = req.session.accountID;
+    if(accountID){
+      const staffIDQuery = "SELECT staffID FROM Account WHERE accountID = ?";
+      const rows = await pool.query(staffIDQuery, [accountID]);
+      const staffID = rows[0].staffID;
+  
+      if(await isAdmin(accountID)){
+        let clientQuery = "SELECT client.* FROM Client client";
+        const rows = await pool.query(clientQuery);
+        return res.send(rows);
+      }
+      else{
+        let clientQuery = "SELECT client.* FROM Client client JOIN StaffClient sc ON client.clientID = sc.clientID WHERE sc.staffID = ?";
+        const rows = await pool.query(clientQuery, [staffID]);
+        return res.send(rows);
+      }
+    }
+    else{
+      return res.send("Invalid authentication");
+    }
+  }
+  catch(err){
+    return res.send("Error retrieving users");
+  }
+  finally{
+    await pool.end();
+  }
+});
+
+/*
+  /api/auth
+
+  Purpose:
+    Receives username and password combo and authenticates user session if credentials are valid.
+*/
+
+app.post('/api/auth', async (req, res) => {
+  const pool = await createTcpPool({ connectionLimit: 5 }); //Initializes connection to DB
+  try{
+    let validation = validateInput(req.body.username, req.body.password)
+    if(!validation.status) return res.send(validation.message);
+
+    let query = "SELECT * FROM Account WHERE username = ?"; 
+    const rows = await pool.query(query, [req.body.username]);
+
+    if(rows.length == 0) res.send("Incorrect username or password"); //No user found
+    else if(rows.length > 1){ //Should not happen
+      console.log("Dangerous query");
+      return res.send("Incorrect username or password");
+    }
+    else if(await bcrypt.compare(req.body.password, rows[0].hash)){
+      req.session.accountID = rows[0].accountID;
+      return res.send("Successful Login");
+    }
+    else{
+      return res.send("Incorrect username or password");
+    }
+  }
+  catch(err){
+    return res.send("Error authenticating");
+  }
+  finally{
+    await pool.end();
+  }
+});
+
+const createUnixSocketPool = async config => {
+  return mysql.createPool({
+    user: process.env.DB_USER,
+    password: process.env.DB_PASS,
+    database: process.env.DB_NAME,
+    socketPath: process.env.INSTANCE_UNIX_SOCKET
+  });
+};
+
+/*
+Checks if a provided accountID is admin.
+This is useful in determining if the user should be able to execute certain operations.
+*/
 async function isAdmin(accountID){
   const pool = await createUnixSocketPool({ connectionLimit: 5 }); //Initializes connection to DB
   try{
@@ -48,45 +139,6 @@ async function isAdmin(accountID){
     await pool.end();
   }
 }
-
-app.post('/api/auth', async (req, res) => {
-  try{
-    let validation = validateInput(req.body.username, req.body.password)
-    if(!validation.status){
-      return res.send(validation.message);
-    }
-
-    const pool = await createUnixSocketPool({ connectionLimit: 5 }); //Initializes connection to DB
-    let query = "SELECT * FROM Account WHERE username = ?"; 
-    const rows = await pool.query(query, [req.body.username]);
-
-    if(rows.length == 0) res.send("Incorrect username or password"); //No user found
-    else if(rows.length > 1){ //Should not happen
-      console.log("Dangerous query");
-      return res.send("Incorrect username or password");
-    }
-    else if(await bcrypt.compare(req.body.password, rows[0].hash)){
-      req.session.accountID = rows[0].accountID;
-      return res.send("Successful Login");
-    }
-    else{
-        return res.send("Incorrect username or password");
-    }
-  }
-  catch(err){
-    console.log(err);
-    return res.send("Error authenticating");
-  }
-});
-
-const createUnixSocketPool = async config => {
-  return mysql.createPool({
-    user: process.env.DB_USER,
-    password: process.env.DB_PASS,
-    database: process.env.DB_NAME,
-    socketPath: process.env.INSTANCE_UNIX_SOCKET
-  });
-};
 
 function validateInput(username, password){
   if(!username || !password){
