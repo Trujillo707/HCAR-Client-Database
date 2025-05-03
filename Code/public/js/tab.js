6cb5f38f230224fffb6d7cbbf5292c45c464b3f9
Jcritt1005 <jcritt9@gmail.com>
2025-05-02 18:00:02 -0700

    HOTFIX: Added 'defer' and 'async' attributes to allow DOM to be loaded correctly in clientDetails.ejs

--- Code/public/js/tab.js
+++ Code/public/js/tab.js
@@ -4,33 +4,39 @@
 /* Apparently its bad practice to send events directly, instead its more modern to
    set up eventListeners
  */
-let list = document.querySelectorAll('.tablinks');
+const list = document.querySelectorAll('.tablinks');
+const backButton = document.getElementById("goBackButton");
 
-list.forEach( currButton => {
-        currButton.addEventListener('click', function () {
-            const theName = this.getAttribute('data-tabName');
-            openTab(theName);
-        })
-    }
-);
+document.addEventListener("DOMContentLoaded", () => {
 
-function openTab(tabName) {
-    let i, tabcontent, tablinks;
-    tabcontent = document.querySelectorAll(".tabcontent");
-    for (i = 0; i < tabcontent.length; i++) {
-        tabcontent[i].style.display = "none";
-    }
-    tablinks = document.querySelectorAll(".tablinks");
-    tablinks.forEach(currLink => {
-        if (currLink.getAttribute('data-tabName') === tabName){
-            currLink.classList.add('active');
+    backButton.addEventListener("click", () => history.back());
+
+    list.forEach( currButton => {
+            currButton.addEventListener('click', function () {
+                const theName = this.getAttribute('data-tabName');
+                openTab(theName);
+            })
         }
-        else{
-            currLink.classList.remove('active');
-        }
-    });
-    document.getElementById(tabName).style.display = "grid";
-}
+    );
 
-// Get the element with id="defaultOpen" and click on it
-document.getElementById("defaultOpen").click();
+    function openTab(tabName) {
+        let i, tabcontent, tablinks;
+        tabcontent = document.querySelectorAll(".tabcontent");
+        for (i = 0; i < tabcontent.length; i++) {
+            tabcontent[i].style.display = "none";
+        }
+        tablinks = document.querySelectorAll(".tablinks");
+        tablinks.forEach(currLink => {
+            if (currLink.getAttribute('data-tabName') === tabName){
+                currLink.classList.add('active');
+            }
+            else{
+                currLink.classList.remove('active');
+            }
+        });
+        document.getElementById(tabName).style.display = "grid";
+    }
+
+    // Get the element with id="defaultOpen" and click on it
+    document.getElementById("defaultOpen").click();
+});
