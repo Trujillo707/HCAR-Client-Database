6ed197e79ce3c3c96aebe6531baf05fc05368aad
Carson Gustafson <carsongustafson@hotmail.com>
2025-05-11 22:46:17 -0700

    Added middle name to search results in the event there are 2 staff with the same name

--- Code/objects/QueryParser.js
+++ Code/objects/QueryParser.js
@@ -699,7 +699,7 @@ export default class QueryParser {
         const fName = typeof req.body.fName === 'string' ? `%${req.body.fName}%` : '%%';
         const lName = typeof req.body.lName === 'string' ? `%${req.body.lName}%` : '%%';
         await connection.beginTransaction();
-        let [staffResults] = await connection.execute("SELECT staffID, fName, lName FROM Staff WHERE fName LIKE ? AND lName LIKE ?", [fName, lName]);
+        let [staffResults] = await connection.execute("SELECT staffID, fName, mName, lName FROM Staff WHERE fName LIKE ? AND lName LIKE ?", [fName, lName]);
         await connection.commit();
         return staffResults;
       }
