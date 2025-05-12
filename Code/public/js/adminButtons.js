13803a5319f1f775261381b59663a0d50503d515
shakylilrat <michael.goodwyn02@gmail.com>
2025-05-12 00:58:32 -0700

    a whole bunch of stuff

--- Code/public/js/adminButtons.js
+++ Code/public/js/adminButtons.js
@@ -1,36 +1,83 @@
-const assignClientsButton = document.getElementById('assignClientsButton');
+/*===
+    adminButtons.js
+    By: Michael Goodwyn
+    Last Modified: 5/11/2025
+
+    Description: This file contains the event listeners for the admin buttons on the admin.ejs page.
+===*/
+
+// Admin function button declarations
+const assignClientButton = document.getElementById('assignClientButton');
 const addEmployeeButton = document.getElementById('addEmployeeButton');
 const removeEmployeeButton = document.getElementById('removeEmployeeButton');
+const addNewClientButton = document.getElementById('addNewClientButton');
+const removeClientButton = document.getElementById('removeClientButton');
 
-assignClientsButton.addEventListener('click', () => {
-    /* set assignClients div to visible, hide the other divs and clear their data */
-    document.getElementById('assignClients').setAttribute("hidden", "false");
+// Admin function div declarations
+const assignClientContent = document.getElementById('assignClient');
+const addEmployeeContent = document.getElementById('addEmployee');
+const removeEmployeeContent = document.getElementById('removeEmployee');
+const addNewClientContent = document.getElementById('addNewClient');
+const removeClientContent = document.getElementById('removeClient');
 
-    document.getElementById('addEmployee').setAttribute("hidden", "true");
-    // document.getElementById('addEmployee').setAttribute("value", "");
+// Sub-divs and buttons for admin functions
+const assignClientSearchButton = document.getElementById('assignClientSearchButton');
+const assignClientResult = document.getElementById('assignClientResult');
 
-    document.getElementById('removeEmployee').setAttribute("hidden", "true");
-    // document.getElementById('removeEmployee').setAttribute("value", "");
+// Initializing admin content to invisible until button is pressed
+assignClientContent.style.display = 'none';
+addEmployeeContent.style.display = 'none';
+removeEmployeeContent.style.display = 'none';
+addNewClientContent.style.display = 'none';
+removeClientContent.style.display = 'none';
+
+// Event listeners for admin function buttons to show their content / hide the others
+assignClientButton.addEventListener('click', () => {
+    assignClientContent.style.display = 'block';
+    addEmployeeContent.style.display = 'none';
+    removeEmployeeContent.style.display = 'none';
+    addNewClientContent.style.display = 'none';
+    removeClientContent.style.display = 'none';
 })
 
 addEmployeeButton.addEventListener('click', () => {
     /* set assignClients div to visible, hide the other divs and clear their data */
-    document.getElementById('addEmployee').setAttribute("hidden", "false");
-
-    document.getElementById('assignClients').setAttribute("hidden", "true");
-    // document.getElementById('addEmployee').setAttribute("value", "");
-
-    document.getElementById('removeEmployee').setAttribute("hidden", "true");
-    // document.getElementById('removeEmployee').setAttribute("value", "");
+    assignClientContent.style.display = 'none';
+    addEmployeeContent.style.display = 'block';
+    removeEmployeeContent.style.display = 'none';
+    addNewClientContent.style.display = 'none';
+    removeClientContent.style.display = 'none';
 })
 
 removeEmployeeButton.addEventListener('click', () => {
     /* set assignClients div to visible, hide the other divs and clear their data */
-    document.getElementById('assignClients').setAttribute("hidden", "false");
+    assignClientContent.style.display = 'none';
+    addEmployeeContent.style.display = 'none';
+    removeEmployeeContent.style.display = 'block';
+    addNewClientContent.style.display = 'none';
+    removeClientContent.style.display = 'none';
+})
 
-    document.getElementById('addEmployee').setAttribute("hidden", "true");
-    // document.getElementById('addEmployee').setAttribute("value", "");
+addNewClientButton.addEventListener('click', () => {
+    /* set assignClients div to visible, hide the other divs and clear their data */
+    assignClientContent.style.display = 'none';
+    addEmployeeContent.style.display = 'none';
+    removeEmployeeContent.style.display = 'none';
+    addNewClientContent.style.display = 'block';
+    removeClientContent.style.display = 'none';
+})
 
-    document.getElementById('removeEmployee').setAttribute("hidden", "true");
-    // document.getElementById('removeEmployee').setAttribute("value", "");
+removeClientButton.addEventListener('click', () => {
+    /* set assignClients div to visible, hide the other divs and clear their data */
+    assignClientContent.style.display = 'none';
+    addEmployeeContent.style.display = 'none';
+    removeEmployeeContent.style.display = 'none';
+    addNewClientContent.style.display = 'none';
+    removeClientContent.style.display = 'block';
+})
+
+/* event listeners for sub-divs in each admin function */
+
+assignClientSearchButton.addEventListener('click', () => {
+    assignClientResult.style.display = 'block';
 })
